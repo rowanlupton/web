aa0c6f694639103f8276ad23085516f09748cad9
rowan <rowanlupton@gmail.com>
2017-08-02 14:40:46 -0400

    renamed app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, url_for, request, render_template, Markup
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/about/')
+def about():
+	return render_template('about.html')
+
+@app.route('/portfolio/')
+def portfolio():
+	return render_template('portfolio.html')
